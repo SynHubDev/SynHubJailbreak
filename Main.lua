c61c4481fc4d92ce81cdd9c6e0e134dd251d513d
SynHubDev <101461514+SynHubDev@users.noreply.github.com>
2022-03-12 14:25:51 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,5 @@
 
+local teleport = require(game:HttpGet("https://github.com/SynHubDev/SynHubTP/blob/main/Main.lua"))
+
+local teleportutils = require(game:HttpGet("https://github.com/SynHubDev/SynHubJailbreak/blob/main/Utils/Teleport.lua"))
+
